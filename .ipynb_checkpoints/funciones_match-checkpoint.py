203aedd4522f98a373647b2df59da98903f4a763
martabalairon <marta.balairon.02@gmail.com>
2024-07-10 13:18:07 +0200

    update code for creating the plot fro checking the ranking

---
+++ .ipynb_checkpoints/funciones_match-checkpoint.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+
+def get_similarity_publication(sim_matrix):
+    '''
+    Function for getting a dataset with the id of each of the publications and the similarity with each of the calls 
+    Pasamos de matriz a dataset
+    
+    sim_matrix -> similarity matrix 
+    '''
+    df = sim_matrix.reset_index()
+
+    df['similarity'] = df.apply(lambda row: row.values[1:], axis=1)
+
+    # Seleccionar solo las columnas 'index' e 'similarity'
+    df = df[['index', 'similarity']]
+
+    # Renombrar las columnas
+    df.columns = ['id', 'similarity']
+    
+    return df
+
+def agg_mean_imp(sim_matrix, df_calls, df_project_publication_researcher):
+    '''
+    Function for computing the aggregated of all the similarities of  an author considering only the not thresholded ones
+    
+    df -> df containing all the similarities without aggreating 
+    '''
+    # check order of the calls and order them if necessary
+    if [str(elemento) for elemento in df_calls['Call'].tolist()] != [str(elemento) for elemento in sim_matrix.columns.tolist()]:
+        column_order = [col for col in [str(elemento) for elemento in df_calls['Call'].tolist()] if col in sim_matrix.columns.tolist()]
+        sim_matrix = sim_matrix[column_order]
+
+        # elimino las calls que no aparecen en mi sim matrix del dataset con calls
+        df_calls['Call'] = df_calls['Call'].astype(str)
+        df_calls = df_calls[df_calls['Call'].isin(sim_matrix.columns.tolist())]  
+
+    df = get_similarity_publication(sim_matrix)
+    
+    df = pd.merge(df_project_publication_researcher, df.rename(columns={'id':'id_paper'}), on='id_paper', how='inner')
+    
+    result_df = pd.DataFrame(columns=['id_RP', 'similarity'])
+    invIDs = df['id_researcher'].unique()
+
+    for invID in tqdm(invIDs, desc="Processing researchers IDs", unit=" id_RP"):
+        df_researcher = df[df['id_researcher'] == invID]
+        df_vectores_sim = pd.DataFrame(df_researcher['similarity'].tolist())
+
+        suma_columnas = df_vectores_sim.sum()
+        non_zero = df_vectores_sim.apply(lambda col: col[col != 0].count())
+        mean_imp = (suma_columnas / non_zero).to_list()
+
+        row = pd.DataFrame(data=[[invID, mean_imp]], columns=['id_RP', 'similarity'])
+        result_df = pd.concat([result_df, row])
+
+    return get_sim_matrix(df_calls, result_df)
+
+def agg_sum(sim_matrix, df_calls, df_project_publication_researcher):
+    '''
+    Function for computing the aggregated of all the similarities of  an author considering only the not thresholded ones
+    
+    df -> df containing all the similarities without aggreating 
+    '''
+    # check order of the calls and order them if necessary
+    if [str(elemento) for elemento in df_calls['Call'].tolist()] != [str(elemento) for elemento in sim_matrix.columns.tolist()]:
+        column_order = [col for col in [str(elemento) for elemento in df_calls['Call'].tolist()] if col in sim_matrix.columns.tolist()]
+        sim_matrix = sim_matrix[column_order]
+
+        # elimino las calls que no aparecen en mi sim matrix del dataset con calls
+        df_calls['Call'] = df_calls['Call'].astype(str)
+        df_calls = df_calls[df_calls['Call'].isin(sim_matrix.columns.tolist())]  
+
+    df = get_similarity_publication(sim_matrix)
+    df = pd.merge(df_project_publication_researcher, df.rename(columns={'id':'id_paper'}), on='id_paper', how='inner')
+    
+    result_df = df.groupby('id_researcher')['similarity'].sum().reset_index()
+    result_df=result_df.rename(columns= {'id_researcher':'id_RP'})
+    
+    return get_sim_matrix(df_calls, result_df)
+
+def agg_mean(sim_matrix, df_calls, df_project_publication_researcher):
+    '''
+    Function for computing the aggregated of all the similarities of  an author considering only the not thresholded ones
+    
+    df -> df containing all the similarities without aggreating 
+    '''
+
+    # check order of the calls and order them if necessary
+    if [str(elemento) for elemento in df_calls['Call'].tolist()] != [str(elemento) for elemento in sim_matrix.columns.tolist()]:
+        column_order = [col for col in [str(elemento) for elemento in df_calls['Call'].tolist()] if col in sim_matrix.columns.tolist()]
+        sim_matrix = sim_matrix[column_order]
+
+        # elimino las calls que no aparecen en mi sim matrix del dataset con calls
+        df_calls['Call'] = df_calls['Call'].astype(str)
+        df_calls = df_calls[df_calls['Call'].isin(sim_matrix.columns.tolist())]  
+
+    df = get_similarity_publication(sim_matrix)
+    df = pd.merge(df_project_publication_researcher, df.rename(columns={'id':'id_paper'}), on='id_paper', how='inner')
+        
+    result_df = df.groupby('id_researcher')['similarity'].apply(lambda x: pd.Series(x.values.tolist()).mean()).reset_index()
+    result_df=result_df.rename(columns= {'id_researcher':'id_RP'})
+
+    return get_sim_matrix(df_calls, result_df)
+
+def get_sim_matrix(df_calls, df_researchers):
+    '''
+    Function for computing the similarity matrix for the researchers and the calls
+    Pasamos de dataset a matriz 
+    
+    df_calls -> dataframe with all the avaliable calls
+    df_researchers -> Dataframe containing the researchers and the similarity with each of the publications
+    '''
+
+    keys_calls = df_calls['Call'].tolist()
+    keys_res = df_researchers['id_RP'].tolist()
+    sim = df_researchers['similarity'].tolist()
+    
+    df = pd.DataFrame(sim, columns=keys_calls, index=keys_res)
+    
+    return df
---
+++ .ipynb_checkpoints/funciones_score_computation-checkpoint.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+
+def get_score_similarity(ranking, id_researcher, call):
+    '''
+    Function for obtaining the score based on the similarity of a recommendation
+
+    ranking -> ranking of recommendations 
+    id_researcher -> target researcher
+    call -> target call
+    '''
+    max_similarity = ranking['similarity'][0]
+
+    # detect if we are recommending researchers or calls
+    if 'id_researcher' in ranking.columns.tolist():
+        similarity = ranking[ranking['id_researcher']==id_researcher].reset_index()['similarity'][0]
+    else:
+        similarity = ranking[ranking['Call']==call].reset_index()['similarity'][0]
+
+    return 1 - ((max_similarity - similarity)/max_similarity)
+
+def get_score_position(ranking, id_researcher, call):
+    '''
+    Function for obtaining the score based on the position of a recommendation in the ranking
+
+    ranking -> ranking of recommendations 
+    id_researcher -> target researcher
+    call -> target call
+    '''
+
+    # detect if we are recommending researchers or calls
+    if 'id_researcher' in ranking.columns.tolist():
+        similarity = ranking[ranking['id_researcher']==id_researcher].reset_index()['similarity'][0]
+        indice_valor_exacto = ranking.loc[ranking['id_researcher'] == id_researcher].index[0]
+        ranking = ranking.iloc[:indice_valor_exacto + 1]
+
+    else:
+        similarity = ranking[ranking['Call']==call].reset_index()['similarity'][0]
+        indice_valor_exacto = ranking.loc[ranking['Call'] == call].index[0]
+        ranking = ranking.iloc[:indice_valor_exacto + 1]
+
+    total_recommendations = ranking.shape[0]
+    posicion = ranking.shape[0]
+
+    if similarity == 0: 
+        return 0 
+    else:
+        return 1 - ((posicion-1) / total_recommendations)
+    
+
+
+def get_score_cluster(ranking, id_researcher, call):
+    '''
+    Function for obtaining the score based on the percentage of clusters or departments in the recommendations matched with the target
+
+    ranking -> ranking of recommendations 
+    id_researcher -> target researcher
+    call -> target call
+    '''
+
+    # detect if we are recommending researchers or calls
+    if 'id_researcher' in ranking.columns.tolist():
+        indice_valor_exacto = ranking.loc[ranking['id_researcher'] == id_researcher].index[0]
+        ranking = ranking.iloc[:indice_valor_exacto + 1]
+        posicion = ranking.shape[0]
+
+        department_correcto = ranking['Department'][posicion-1]
+        count_department_correctos = 0
+        for i in range(ranking.shape[0]):
+            if ranking['Department'][i] == department_correcto:
+                count_department_correctos += 1
+
+        return count_department_correctos/ranking.shape[0] 
+
+
+    else:
+        indice_valor_exacto = ranking.loc[ranking['Call'] == call].index[0]
+        ranking = ranking.iloc[:indice_valor_exacto + 1]
+        posicion = ranking.shape[0]
+
+        cluster_correcto = ranking['Work Programme'][posicion-1]
+        count_cluster_correctos = 0
+        for i in range(ranking.shape[0]):
+            if ranking['Work Programme'][i] == cluster_correcto:
+                count_cluster_correctos += 1
+
+        return count_cluster_correctos/ranking.shape[0]
+
+        
+
+
+
