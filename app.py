2d59ad14f0bb572348a0fdc5f5e3469741e3408f
Marta Almagro Fuello <mafuello@hator00.tsc.uc3m.es>
2024-07-17 11:27:40 +0200

    created plots

--- app.py
+++ app.py
@@ -38,6 +38,15 @@ researchers_sum_bhattacharyya, researchers_mean_bhattacharyya, researchers_mean_
 researchers_sum_separated, researchers_mean_separated, researchers_mean_imp_separated, calls_sum_separated, calls_mean_separated, calls_mean_imp_separated = obtain_data_cached(df_val, agg_methods, 'separated', df_researchers, df_calls)
 researchers_sum_semiseparated, researchers_mean_semiseparated, researchers_mean_imp_semiseparated, calls_sum_semiseparated, calls_mean_semiseparated, calls_mean_imp_semiseparated = obtain_data_cached(df_val, agg_methods, 'semiseparated', df_researchers, df_calls)
 
+# Supongamos que scores_researchers y scores_calls están definidos en alguna parte
+# Aquí se simulan los datos para el ejemplo
+scores_researchers = np.random.rand(3, 3)  # Reemplaza con los datos reales
+scores_calls = np.random.rand(3, 3)  # Reemplaza con los datos reales
+
+# Crear los DataFrames de resultados
+results_researchers = pd.DataFrame(index=agg_methods, columns=['score_similarity', 'score_position', 'score_department'], data=scores_researchers)
+results_calls = pd.DataFrame(index=agg_methods, columns=['score_similarity', 'score_position', 'score_cluster'], data=scores_calls)
+
 # Tabs para comparar métodos de agregación y métodos de recomendación
 tab1, tab2 = st.tabs(["Compare aggregation methods", "Compare recommendations methods"])
 with tab1:
@@ -45,10 +54,9 @@ with tab1:
     selectbox_key1 = st.empty()  # Genera un contenedor vacío para poder actualizar el selectbox más tarde
     with selectbox_key1:
         recommendations = st.selectbox('Select which recommendations to obtain:', ['Researchers', 'Calls'], key='selectbox_tab1')  # Añade una key única
-        
 
     # Título de la aplicación
-    st.title('Comparison of different aggregation methods  when recommending {}'.format(recommendations))
+    st.title('Comparison of different aggregation methods when recommending {}'.format(recommendations))
     col1, col2 = st.columns(2)
     col3, col4 = st.columns(2)
 
@@ -58,8 +66,10 @@ with tab1:
         
         if recommendations == 'Researchers':
             get_plot_comparison_agg_methods(ax, method, researchers_sum_bert, researchers_mean_bert, researchers_mean_imp_bert)
+            st.table(results_researchers)
         elif recommendations == 'Calls':
             get_plot_comparison_agg_methods(ax, method, calls_sum_bert, calls_mean_bert, calls_mean_imp_bert)
+            st.table(results_calls)
 
         st.pyplot(fig)
 
@@ -69,9 +79,11 @@ with tab1:
         
         if recommendations == 'Researchers':
             get_plot_comparison_agg_methods(ax, method, researchers_sum_bhattacharyya, researchers_mean_bhattacharyya, researchers_mean_imp_bhattacharyya)
+            st.table(results_researchers)
         elif recommendations == 'Calls':
-            get_plot_comparison_agg_methods(ax, method, calls_sum_bhattacharyya, calls_mean_bhattacharyya, calls_mean_imp_bhattacharyya)    
-        
+            get_plot_comparison_agg_methods(ax, method, calls_sum_bhattacharyya, calls_mean_bhattacharyya, calls_mean_imp_bhattacharyya)
+            st.table(results_calls)
+
         st.pyplot(fig)
 
     with col3:
@@ -80,9 +92,11 @@ with tab1:
         
         if recommendations == 'Researchers':
             get_plot_comparison_agg_methods(ax, method, researchers_sum_separated, researchers_mean_separated, researchers_mean_imp_separated)
+            st.table(results_researchers)
         elif recommendations == 'Calls':
-            get_plot_comparison_agg_methods(ax, method, calls_sum_separated, calls_mean_separated, calls_mean_imp_separated)    
-        
+            get_plot_comparison_agg_methods(ax, method, calls_sum_separated, calls_mean_separated, calls_mean_imp_separated)
+            st.table(results_calls)
+
         st.pyplot(fig)
 
     with col4:
@@ -91,9 +105,11 @@ with tab1:
         
         if recommendations == 'Researchers':
             get_plot_comparison_agg_methods(ax, method, researchers_sum_semiseparated, researchers_mean_semiseparated, researchers_mean_imp_semiseparated)
+            st.table(results_researchers)
         elif recommendations == 'Calls':
             get_plot_comparison_agg_methods(ax, method, calls_sum_semiseparated, calls_mean_semiseparated, calls_mean_imp_semiseparated)
-        
+            st.table(results_calls)
+
         st.pyplot(fig)
 
 with tab2:
@@ -103,37 +119,31 @@ with tab2:
         recommendations = st.selectbox('Select which recommendations to obtain:', ['Researchers', 'Calls'], key='selectbox_tab2')  # Añade una key única
     
     # Título de la aplicación
-    st.title('Comparison of different methods  when recommending {}'.format(recommendations))
+    st.title('Comparison of different methods when recommending {}'.format(recommendations))
 
-    with st.columns(1)[0]:
-        fig, ax = plt.subplots(figsize=(15, 10))  
-        agg_method = agg_methods[0]
-        
-        if recommendations == 'Researchers':
-            get_plot_comparison_methods(ax, agg_method, researchers_sum_bert, researchers_sum_bhattacharyya, researchers_sum_separated, researchers_sum_semiseparated)
-        elif recommendations == 'Calls':
-            get_plot_comparison_methods(ax, agg_method, calls_sum_bert, calls_sum_bhattacharyya, calls_sum_separated, calls_sum_semiseparated)
+    for agg_method in agg_methods:
+        with st.columns(1)[0]:
+            fig, ax = plt.subplots(figsize=(15, 10))  
+            
+            if recommendations == 'Researchers':
+                if agg_method == 'sum':
+                    get_plot_comparison_methods(ax, agg_method, researchers_sum_bert, researchers_sum_bhattacharyya, researchers_sum_separated, researchers_sum_semiseparated)
+                    st.table(results_researchers)
+                elif agg_method == 'mean':
+                    get_plot_comparison_methods(ax, agg_method, researchers_mean_bert, researchers_mean_bhattacharyya, researchers_mean_separated, researchers_mean_semiseparated)
+                    st.table(results_researchers)
+                elif agg_method == 'mean_imp':
+                    get_plot_comparison_methods(ax, agg_method, researchers_mean_imp_bert, researchers_mean_imp_bhattacharyya, researchers_mean_imp_separated, researchers_mean_imp_semiseparated)
+                    st.table(results_researchers)
+            elif recommendations == 'Calls':
+                if agg_method == 'sum':
+                    get_plot_comparison_methods(ax, agg_method, calls_sum_bert, calls_sum_bhattacharyya, calls_sum_separated, calls_sum_semiseparated)
+                    st.table(results_calls)
+                elif agg_method == 'mean':
+                    get_plot_comparison_methods(ax, agg_method, calls_mean_bert, calls_mean_bhattacharyya, calls_mean_separated, calls_mean_semiseparated)
+                    st.table(results_calls)
+                elif agg_method == 'mean_imp':
+                    get_plot_comparison_methods(ax, agg_method, calls_mean_imp_bert, calls_mean_imp_bhattacharyya, calls_mean_imp_separated, calls_mean_imp_semiseparated)
+                    st.table(results_calls)
 
-        st.pyplot(fig)
-
-    with st.columns(1)[0]:
-        fig, ax = plt.subplots(figsize=(15, 10))  
-        agg_method = agg_methods[1]
-        
-        if recommendations == 'Researchers':
-            get_plot_comparison_methods(ax, agg_method, researchers_mean_bert, researchers_mean_bhattacharyya, researchers_mean_separated, researchers_mean_semiseparated)
-        elif recommendations == 'Calls':
-            get_plot_comparison_methods(ax, agg_method, calls_mean_bert, calls_mean_bhattacharyya, calls_mean_separated, calls_mean_semiseparated)
-
-        st.pyplot(fig)
-
-    with st.columns(1)[0]:
-        fig, ax = plt.subplots(figsize=(15, 10))  
-        agg_method = agg_methods[2]
-        
-        if recommendations == 'Researchers':
-            get_plot_comparison_methods(ax, agg_method, researchers_mean_imp_bert, researchers_mean_imp_bhattacharyya, researchers_mean_imp_separated, researchers_mean_imp_semiseparated)
-        elif recommendations == 'Calls':
-            get_plot_comparison_methods(ax, agg_method, calls_mean_imp_bert, calls_mean_imp_bhattacharyya, calls_mean_imp_separated, calls_mean_imp_semiseparated)
-
-        st.pyplot(fig)
+            st.pyplot(fig)
